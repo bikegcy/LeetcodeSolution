4a87b06065c205098125f25d190135c4848e3d6a
Gao Chaoyu <bikegcy@gmail.com>
2016-05-01 23:05:48 +0200

    Update 300-LongestIncreasingSubsequence.java

--- 300-LongestIncreasingSubsequence.java
+++ 300-LongestIncreasingSubsequence.java
@@ -27,4 +27,38 @@ public class Solution {
         }
         return MaxLIS;
     }
+    /*
+    public int lengthOfLIS(int[] nums) {
+	    if (nums == null) {
+	        return 0;
+	    }
+	    
+	    List<Integer> seq = new ArrayList();
+	    for (int i = 0; i < nums.length; i++) {
+	        updateSequence(seq, nums[i]);
+	    }
+	    
+	    return seq.size();
+	}
+	
+	private void updateSequence(List<Integer> seq, int elem) {
+	    int start = 0, end = seq.size() - 1;
+	    while (start <= end) {
+	        int mid = start + (end - start) / 2;
+	        if (seq.get(mid) == elem) {
+	            return;
+	        } else if (seq.get(mid) < elem) {
+	            start = mid + 1;
+	        } else {
+	            end = mid - 1;
+	        }
+	    }
+	    
+	    if (start >= seq.size()) {
+	        seq.add(elem);
+	    } else {
+	        seq.set(start, elem);
+	    }
+	}
+	*/
 }

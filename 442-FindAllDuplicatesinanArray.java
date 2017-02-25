2c30d97fc6e7eb0161ef10a41c73683583ccb4a5
bikegcy <bikegcy@gmail.com>
2017-02-24 22:46:30 -0800

    new algorithm no extra space

--- 442-FindAllDuplicatesinanArray.java
+++ 442-FindAllDuplicatesinanArray.java
@@ -3,8 +3,10 @@
  * @cgao15@ucsc.edu
  * @LeetcodeSolution
  * @Use an array res to store [1...n] once appear let it be 0
- * @If res[i] == 0, add it to result List
- */
+ * @If res[i] == 0, add it to result Listi
+ * @New algorithm: let nums[nums[i] - 1] = - nums[nums[i] - 1]to mark
+ * @If it is positive then it appears twice.
+ *
 public class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         int Length  = nums.length;
@@ -25,3 +27,20 @@ public class Solution {
         return result;
     }
 }
+*/
+public class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> result = new ArrayList<Integer>();
+        for(int i = 0;i < nums.length;i++){
+            int temp = Math.abs(nums[i]) - 1;
+            nums[temp] = - nums[temp];
+            if(nums[temp] > 0){
+                result.add(temp + 1);
+            }
+        }
+        return result;
+    }
+}
+
+
+

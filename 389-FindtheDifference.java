eec6272fecba44a54e93b73a4fb1440c17b1e4c8
bikegcy <bikegcy@gmail.com>
2017-02-25 16:33:51 -0800

    new algorithm to decrease time

--- 389-FindtheDifference.java
+++ 389-FindtheDifference.java
@@ -7,6 +7,7 @@
  */
 public class Solution {
     public char findTheDifference(String s, String t) {
+        /*  11ms My method at the first time
         char[] temps = s.toCharArray();
         char[] tempt = t.toCharArray();
         Arrays.sort(temps);
@@ -19,5 +20,25 @@ public class Solution {
                 return newt.charAt(i);
         }
         return newt.charAt(i);
+        */
+        /*  10ms:bit Manipulation
+        int n = s.length();
+        char res = t.charAt(n);
+        for(int i = 0;i < n;i++){
+            res ^= s.charAt(i);
+            res ^= t.charAt(i);
+        }
+        return res;
+        */
+        //6ms
+        int n = s.length();
+        int res = (int)t.charAt(n);
+        for(int i = 0;i < n;i++){
+            res -= (int)s.charAt(i);
+            res += (int)t.charAt(i);
+        }
+        return (char)res;
+        
+        
     }
 }

7c2942150f5ed76c116adfb11d441aa5762042d9
bikegcy <bikegcy@gmail.com>
2017-02-26 16:11:59 -0800

    new algorithm makes sense

--- 258-AddDigits.java
+++ 258-AddDigits.java
@@ -6,9 +6,13 @@
  */
 public class Solution {
     public int addDigits(int num) {
+        /*
         if(num < 10)
             return num;
         num = ((num - 10) % 90) % 9 + 1;
+        return num;
+        */
+        num = num - 9 * ((num - 1) / 9);
         return num;
     }
 }

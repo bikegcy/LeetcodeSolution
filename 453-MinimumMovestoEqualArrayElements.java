68a87ef89188086a862193d2d7bb594d586ec33b
bikegcy <bikegcy@gmail.com>
2017-03-01 10:57:48 -0800

    much easier method

--- 453-MinimumMovestoEqualArrayElements.java
+++ 453-MinimumMovestoEqualArrayElements.java
@@ -5,9 +5,13 @@
  * @First sort the array.
  * @Difine the manipulation,assert: use numofsame * (nums[i] - nums[i - 1])
  * @times of manipulaiton to make sure one more number is the biggest same.
+ * @New much faster algorithm:
+ * @To add 1 for n - 1 is the same of minus one for certain number.
+ * @Hence find the smallest number and result = sum - min * n.
  */
 public class Solution {
     public int minMoves(int[] nums) {
+        /*
         int result = 0,numofsame = 1;
         int numlength = nums.length;
         int index = numlength - 1;
@@ -22,6 +26,15 @@ public class Solution {
             index--;
             numofsame++;
         }
+        return result;
+        */
+        int result = 0,sum = 0,min = Integer.MAX_VALUE;
+        for(int element:nums){
+            if(element < min)
+                min = element;
+            sum += element;
+        }
+        result = sum - min * nums.length;
         return result;
     }
 }

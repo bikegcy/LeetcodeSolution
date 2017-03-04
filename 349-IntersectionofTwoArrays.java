335131f9cf0d85c18a9c7ba6737424c31d73afce
bikegcy <bikegcy@gmail.com>
2017-03-03 18:04:07 -0800

    hashset faster

--- 349-IntersectionofTwoArrays.java
+++ 349-IntersectionofTwoArrays.java
@@ -3,9 +3,11 @@
  * @cgao15@ucsc.edu
  * @LetcodeSolution
  * @Use hashmap to store the freq.
+ * @Use HashSet: no duplication.
  */
 public class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
+        /*
         if(nums1.length == 0 || nums2.length == 0)
             return new int[0];
         Map<Integer,Integer> numsmap = new HashMap<Integer,Integer>();
@@ -28,5 +30,23 @@ public class Solution {
             result[index] = nums2[index];
         }
         return result;
+        */
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> intersect = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            set.add(nums1[i]);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                intersect.add(nums2[i]);
+            }
+        }
+        int[] result = new int[intersect.size()];
+        int i = 0;
+        for (Integer num : intersect) {
+            result[i++] = num;
+        }
+        return result;
+    
     }
 }

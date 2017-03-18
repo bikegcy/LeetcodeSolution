b82869880d508597f3b2c4594c273d72e0f8a34f
bikegcy <bikegcy@gmail.com>
2017-03-18 15:26:30 -0700

    understand the peoblem

--- 287-FindtheDuplicateNumber.java
+++ 287-FindtheDuplicateNumber.java
@@ -6,20 +6,19 @@
  */
 public class Solution {
     public int findDuplicate(int[] nums) {
-        if (nums.length > 1){
-		    int slow = nums[0];
-		    int fast = nums[nums[0]];
-		    while (slow != fast){
-			    slow = nums[slow];
-			    fast = nums[nums[fast]];
-		    }
-		    fast = 0;
-		    while (fast != slow){
-			    fast = nums[fast];
-			    slow = nums[slow];
-		    }
-		    return slow;
-	    }
-	    return -1;
+        if(nums == null || nums.length <= 2)
+            return 1;
+        int fast = nums[nums[0]];
+        int slow = nums[0];
+        while(fast != slow){
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        }
+        slow = 0;
+        while(fast != slow){
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        return fast;
     }
 }

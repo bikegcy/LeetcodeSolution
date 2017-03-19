18bc51c25e1581f82da4d15acb3f99a66b632de6
bikegcy <bikegcy@gmail.com>
2017-03-19 11:57:40 -0700

    faster algo

--- 337-HouseRobberIII.java
+++ 337-HouseRobberIII.java
@@ -13,11 +13,33 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class Solution {
     public int rob(TreeNode root) {
         if(root == null)
             return 0;
-        int result = helper(root);
+        int[] result = new int[2];
+        result = helper2(root);
+        //int result = helper(root);
+        return Math.max(result[0],result[1]);
+    }
+    
+    private int[] helper2(TreeNode root){
+        if(root == null)
+            return new int[2];
+        int[] left = helper2(root.left);
+        int[] right = helper2(root.right);
+        int[] result = new int[2];
+        result[0] = Math.max(left[0],left[1]) + Math.max(right[0],right[1]);
+        result[1] = root.val + left[0] + right[0];
         return result;
     }
     
